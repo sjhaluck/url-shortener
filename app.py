7f980458926e2e463fb9803e2230010524428c19
Scott Haluck <sjhaluck@gmail.com>
2021-06-08 11:04:36 -0500

    feat: serving static files and redirecting to appropriate url for short codes, custom error handling

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
+import json
+import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'gafdagat89482094815fdaklfj'
 
 @app.route('/')
 def home():
@@ -9,6 +13,43 @@ def home():
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as url_file:
+                urls = json.load(url_file)
+
+        if request.form['code'] in urls.keys():
+            flash('Short Name has already been used. Please select a unique\
+             Short Name.')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url':request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save(os.path.join('C:/Users/sjhal/Desktop/url-shortener/static/user_files',full_name))
+            urls[request.form['code']] = {'file':full_name}
+
+        with open('urls.json','w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return 'This is not valid.'
+        return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as url_file:
+            urls = json.load(url_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
+                else:
+                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
